6d406a28bc978545cb4ee76e7c189f3cf50b2e53
FabiolaBaani <33585451+FabiolaBaani@users.noreply.github.com>
2023-08-23 14:57:22 -0300

    Update botpoliteia.js

--- botpoliteia.js
+++ botpoliteia.js
@@ -308,29 +308,31 @@ client.on('message', async msg => {
   } 
   
   else if (msg.body !== null && msg.body === "10") {
-   msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
   else if (msg.body !== null && msg.body === "11") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
    }
   
   else if (msg.body !== null && msg.body === "12") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
  else if (msg.body !== null && msg.body === "13") {
+  msg.reply("Opção inválida"); 
   }
   
  else if (msg.body !== null && msg.body === "14") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
 
   else if (msg.body !== null && msg.body === "15") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
 	  
 else if (msg.body !== null && msg.body === "16"){
+  msg.reply("Opção inválida"); 
   }
     
   else if (msg.body !== null && msg.body === "17") {
@@ -349,32 +351,31 @@ else if (msg.body !== null && msg.body === "16"){
   //} 
   
   else if (msg.body !== null && msg.body === "18") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
   else if (msg.body !== null && msg.body === "19") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
   else if (msg.body !== null && msg.body === "20") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
   else if (msg.body !== null && msg.body === "21") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
   
   else if (msg.body !== null && msg.body === "22") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
 
   else if (msg.body !== null && msg.body === "23") {
-    msg.reply("");
+  msg.reply("Opção inválida"); 
   }
 
   else if (msg.body !== null && msg.body === "24") {
-    msg.reply("");
-	  
+  msg.reply("Opção inválida"); 
   }
 
   else if (msg.body !== null || msg.body === "0" || msg.type === 'ptt' || msg.hasMedia) {
@@ -389,7 +390,7 @@ else if (msg.body !== null && msg.body === "16"){
       }
 		});
     delay(12000).then(async function() {
-      const saudacao = ("Este é um atendimento automático. Caso queira resolver sua dúvida por aqui agora mesmo, digite o número de uma das opções abaixo para iniciarmos nossa conversa: ⬇️\r\n\r\n*1* - Quero saber como funciona esse robô de WhatsApp \r\n*2* - Quais equipamentos preciso ter para iniciar este serviço? \r\n*3* - Terei que instalar ou configurar alguma coisa? \r\n*4* - Funciona no Brasil todo?\r\n*5* - Este robô responde as mensagens enviadas pelos clientes?\r\n*6* - Preciso estar com meu celular conectado na internet para que o sistema funcione? \r\n*7* - Preciso ter um número exclusivo para ativar o atendimento robotizado? \r\n*8* - Quanto custa este serviço?  \r\n*9* - Minha dúvida não está aqui, quero falar com alguém da Politeia");
+      const saudacao = ("Este é um atendimento *digital*.\nCaso queira resolver sua dúvida por aqui mesmo, digite o número de uma das opções abaixo para iniciarmos nossa conversa: ⬇️\r\n\r\n*1* - Quero saber como funciona esse robô de WhatsApp \r\n*2* - Quais equipamentos preciso ter para iniciar este serviço? \r\n*3* - Terei que instalar ou configurar alguma coisa? \r\n*4* - Funciona no Brasil todo?\r\n*5* - Este robô responde as mensagens enviadas pelos clientes?\r\n*6* - Preciso estar com meu celular conectado na internet para que o sistema funcione? \r\n*7* - Preciso ter um número exclusivo para ativar o atendimento robotizado? \r\n*8* - Quanto custa este serviço?  \r\n*9* - Minha dúvida não está aqui, quero falar com alguém da Politeia");
 	     client.sendMessage(msg.from, saudacao)
      		});
     
